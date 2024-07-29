d2d9a07d4ca0c4512f23c0aa8d81986631cc59b5
tangwai <tmichael11314@gmail.com>
2024-07-29 08:19:26 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # basic-site-node
+
+This my first node project.
+
+Just an intro to using Node--things that I learned:
+
+    how to use Node packages to read/create files
+    routing files using Node
+    establishing a remote server
---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+    const extname = path.extname(filePath);
+
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+}).listen(8080, () => {
+    console.log('Server running on port 8080');
+});
